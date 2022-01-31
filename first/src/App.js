2360f83500bc697c36553141434e94d8f99f6e7c
swadesh soni <swadeshishu2001@gmail.com>
2022-01-31 15:42:32 +0530

    starting

---
+++ first/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Navbar from './components/navbar/navbar'
+import Footer from './components/footer/footer'
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+     <h1>everything will work now </h1>
+     <Footer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ first/src/components/footer/footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './footer.css'
+
+const footer = () => {
+  return <>
+        <div className="header">
+            <h2>hell of a job</h2>
+        </div>
+  </>;
+};
+
+export default footer;
